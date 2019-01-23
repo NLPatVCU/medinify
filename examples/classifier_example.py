9b086c7ea784922402e39570fe63b057c0fcb677
Jorge Vargas <vargasja2@vcu.edu>
2019-01-23 14:45:58 -0500

    Comment out decision tree example

--- examples/classifier_example.py
+++ examples/classifier_example.py
@@ -8,17 +8,15 @@ def main():
     """ Main function.
     """
 
-    """ Naive bayes classifier
-    """
+    # Naive bayes classifier
     review_classifier = ReviewClassifier('nb', 'stopwords.txt')
     review_classifier.train('citalopram_train.csv')
     review_classifier.classify('neutral.txt')
 
-    """ Decision tree classifier
-    """
-    review_classifier = ReviewClassifier('dt', 'stopwords.txt')
-    review_classifier.train('citalopram_train.csv')
-    review_classifier.classify('neutral.txt')
+    # Decision tree classifier
+    # review_classifier = ReviewClassifier('dt', 'stopwords.txt')
+    # review_classifier.train('citalopram_train.csv')
+    # review_classifier.classify('neutral.txt')
 
 if __name__ == "__main__":
     main()
