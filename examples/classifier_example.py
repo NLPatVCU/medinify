e694d2280d19a937667b9c8056c484ce12a1fd02
Gabby Gurdin <ggurdin@gmail.com>
2019-01-23 14:12:14 -0500

    Fixed issues with classifier examples

--- examples/classifier_example.py
+++ examples/classifier_example.py
@@ -4,24 +4,17 @@ Examples for how to use the Medinify package
 
 from medinify.sentiment.review_classifier import ReviewClassifier
 
-""" Naive bayes classifier
-"""
-
 def main():
     """ Main function.
+    """
+
+    """ Naive bayes classifier
     """
     review_classifier = ReviewClassifier('nb', 'stopwords.txt')
     review_classifier.train('citalopram_train.csv')
     review_classifier.classify('neutral.txt')
 
-if __name__ == "__main__":
-    main()
-
-""" Decision tree classifier
-"""
-
-def main():
-    """ Main function.
+    """ Decision tree classifier
     """
     review_classifier = ReviewClassifier('dt', 'stopwords.txt')
     review_classifier.train('citalopram_train.csv')
