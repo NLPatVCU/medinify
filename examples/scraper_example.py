dfdaecba26dfce7778486479ffd8830492f0cd4e
Jorge Vargas <vargasja2@vcu.edu>
2019-01-24 15:07:06 -0500

    Add max_pages method to webmd scraper

--- examples/scraper_example.py
+++ examples/scraper_example.py
@@ -8,13 +8,13 @@ from medinify.scrapers import DrugsScraper
 def main():
     """ Main function.
     """
-    # input_url = 'https://www.webmd.com/drugs/drugreview-12120-Doxil+intravenous.aspx?drugid=12120&drugname=Doxil+intravenous&pageIndex=1&sortby=3&conditionFilter=-1'
-    # webmd_scraper = WebMDScraper("citalopram_train.csv")
-    # webmd_scraper.scrape(input_url, 10)
+    input_url = 'https://www.webmd.com/drugs/drugreview-1701-citalopram-oral.aspx?drugid=1701&drugname=citalopram-oral'
+    webmd_scraper = WebMDScraper()
+    webmd_scraper.scrape(input_url, 'test.csv')
 
-    drugs_url = 'https://www.drugs.com/comments/dabigatran/'
-    drugs_scraper = DrugsScraper()
-    drugs_scraper.scrape(drugs_url, 'dabigatran.csv', 4)
+    # drugs_url = 'https://www.drugs.com/comments/dabigatran/'
+    # drugs_scraper = DrugsScraper()
+    # drugs_scraper.scrape(drugs_url, 'dabigatran.csv', 4)
 
 if __name__ == "__main__":
     main()
--- medinify/scrapers/webmd_scraper.py
+++ medinify/scrapers/webmd_scraper.py
@@ -20,11 +20,13 @@ class WebMDScraper():
         output_path (str) : CSV file to output scraped information        
     """
 
-    output_path = ""
+    all_pages = True
+    pages = 1
+    review_list = []
 
-    def __init__(self, output_path):
-        self.output_path = output_path
-
+    def __init__(self, all_pages=True, pages=1):
+        self.all_pages = all_pages
+        self.pages = pages
 
     def clean_comment(self, comment):
         """Cleans comment for proper CSV usage.
@@ -37,8 +39,47 @@ class WebMDScraper():
         comment = ' '.join(comment.splitlines())
         return comment
 
+    def max_pages(self, input_url):
+        """Finds number of review pages for this drug.
 
-    def scrape(self, input_url, pages):
+        Args:
+            input_url: URL for the first page of reviews.
+        Returns:
+            (int) Highest page number
+        """
+        page = requests.get(input_url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        total_reviews_text = soup.find('span', {'class': 'totalreviews'}).text
+        total_reviews = [int(s) for s in total_reviews_text.split() if s.isdigit()][0]
+        max_pages = total_reviews // 5
+        print('Found ' + str(total_reviews) + ' reviews.')
+        print('Scraping ' + str(max_pages) + ' pages...')
+        return max_pages
+
+    def scrape_page(self, page_url):
+        """Scrapes a single page for reviews and adds them to review_list
+
+        Args:
+            page_url: URL of the page to scrape.
+        """
+        page = requests.get(page_url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        reviews = soup.find_all('div', attrs={'class': 'userPost'})
+
+        for review in reviews:
+            comment = review.find('p', id=re.compile("^comFull*")).text
+            comment = self.clean_comment(comment)
+            if comment:
+                ratings = review.find_all('span', attrs={'class': 'current-rating'})
+                calculated_rating = 0.0
+
+                for rating in ratings:
+                    calculated_rating += int(rating.text.replace('Current Rating:', '').strip())
+
+                calculated_rating = calculated_rating / 3.0
+                self.review_list.append({'comment': comment, 'rating': calculated_rating})
+
+    def scrape(self, input_url, output_path):
         """Scrapes the reviews from WebMD
 
         Args:
@@ -46,34 +87,28 @@ class WebMDScraper():
             pages (int) : number of pages to scrape
         """
 
+        self.review_list = []
+
         quote_page1 = input_url + '&pageIndex='
         quote_page2 = '&sortby=3&conditionFilter=-500'
-    
-        num_pages = pages
-        review_list = []
+        num_pages = 0
+
+        if self.all_pages:
+            num_pages = self.max_pages(input_url)
+        else:
+            num_pages = self.pages
 
         for i in range(num_pages):
-            url = quote_page1 + str(i) + quote_page2
-            page = requests.get(url)
-            soup = BeautifulSoup(page.text, 'html.parser')
-            reviews = soup.find_all('div', attrs={'class': 'userPost'})
+            page_url = quote_page1 + str(i) + quote_page2
+            self.scrape_page(page_url)
 
-            for review in reviews:
-                comment = review.find('p', id=re.compile("^comFull*")).text
-                comment = self.clean_comment(comment)
-                if comment:
-                    ratings = review.find_all('span', attrs={'class': 'current-rating'})
-                    calculated_rating = 0.0
+            page = i + 1
+            if page % 10 == 0:
+                print('Scraped ' + str(page) + ' pages...')
 
-                    for rating in ratings:
-                        calculated_rating += int(rating.text.replace('Current Rating:', '').strip())
-
-                    calculated_rating = calculated_rating / 3.0
-                    review_list.append({'comment': comment, 'rating': calculated_rating})
-
-        with open(self.output_path, 'w') as output_file:
+        with open(output_path, 'w') as output_file:
             dict_writer = csv.DictWriter(output_file, ['comment', 'rating'])
             dict_writer.writeheader()
-            dict_writer.writerows(review_list)
+            dict_writer.writerows(self.review_list)
 
-        print('Reviews scraped: ' + str(len(review_list)))
+        print('Reviews scraped: ' + str(len(self.review_list)))
