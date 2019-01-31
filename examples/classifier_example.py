5b6a96bae7f9e710f8a4119ae60ff3171a68eda5
Jorge Vargas <vargasja2@vcu.edu>
2019-01-31 14:01:43 -0500

    Fix import issues in sentiment package and improve example

--- examples/classifier_example.py
+++ examples/classifier_example.py
@@ -3,7 +3,7 @@ Examples for how to use the Medinify package
 """
 
 from medinify.sentiment import ReviewClassifier
-from medinify.sentiment import NeuralNetReviewClassifier
+# from medinify.sentiment import NeuralNetReviewClassifier
 
 def main():
     """ Main function.
@@ -11,8 +11,8 @@ def main():
 
     # Naive bayes classifier
     review_classifier = ReviewClassifier('nb', 'stopwords.txt')
-    review_classifier.train('citalopram_train.csv')
-    review_classifier.classify('neutral.txt')
+    review_classifier.train('citalopram-reviews.csv')
+    # review_classifier.classify('neutral.txt')
 
     # Decision tree classifier
     # review_classifier = ReviewClassifier('dt', 'stopwords.txt')
--- medinify/sentiment/__init__.py
+++ medinify/sentiment/__init__.py
@@ -1,7 +1,7 @@
 """Configure medinify.scrapers
 """
 from medinify.sentiment.review_classifier import ReviewClassifier
-from medinify.sentiment.review_classifier import NeuralNetReviewClassifier
+from medinify.sentiment.nn_review_classifier import NeuralNetReviewClassifier
 
 __all__ = (
     'ReviewClassifier',
