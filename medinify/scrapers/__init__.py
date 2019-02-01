9fbf15a74e2894e96fc709a495da478347b010a9
nrhwest <nrhwest@gmail.com>
2019-01-31 22:55:41 -0500

    finished everydayhealth-scraper

--- medinify/scrapers/__init__.py
+++ medinify/scrapers/__init__.py
@@ -3,9 +3,11 @@
 from medinify.scrapers.webmd_scraper import WebMDScraper
 from medinify.scrapers.drugs_scraper import DrugsScraper
 from medinify.scrapers.drugratingz_scraper import DrugRatingzScraper
+from medinify.scrapers.everydayhealth_scraper import EverydayHealthScraper
 
 __all__ = (
     'WebMDScraper',
     'DrugsScraper',
     'DrugRatingzScraper',
+    'EverydayHealthScraper',
 )
