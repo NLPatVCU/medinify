bcca39d56acbac4cc4d939a4fdcecdf80eca303b
Farhana Khan <farhanakhan@Farhanas-MacBook-Air.local>
2019-02-15 10:42:01 -0500

    modified tests.py

--- tests/test_scrapers.py
+++ tests/test_scrapers.py
@@ -1,6 +1,10 @@
-from medinify.scrapers import WebMDScraper
+#from medinify.scrapers import WebMDScraper
+from medinify.scrapers import IodineScraper
+from medinify.scrapers import DrugRatingzScraper
+from medinify.scrapers import DrugsScraper
 
 
+'''
 def test_webmd_max_pages():
     input_url = 'https://www.webmd.com/drugs/drugreview-151652-banzel.aspx?drugid=151652&drugname=banzel'
     webmd_scraper = WebMDScraper()
@@ -19,3 +23,35 @@ def test_webmd_scrape():
     webmd_scraper.scrape(input_url)
     assert len(webmd_scraper.review_list) > 5
     assert 'comment' and 'effectiveness' and 'satisfaction' in webmd_scraper.review_list[-1]
+    '''
+
+#Test for iodine.com 
+def test_iodine_scrape(): 
+    input_url = 'https://www.iodine.com/drug/adderall/reviews'
+    iodine_scraper = IodineScraper()
+    iodine_scraper.scraper(input_url)
+    assert iodine_scraper.review_list
+    assert 'comment' and 'worth it' and 'worked well' and 'big hassle' in iodine_scraper.review_list[-1]
+
+#Test for drugratingz.com
+#need to review 
+def test_drugratingz_scrape():
+    input = 'https://www.drugratingz.com/reviews/75/Drug-Adderall-XR.html'
+    output_path = 'drugRatingz.csv' 
+    drug_scraper = DrugRatingzScraper()
+    drug_scraper.scrape(input, output_path)
+    assert drug_scraper.review_list
+    assert 'comment' and 'effectiveness' and 'no side effects' and 'convenience' and 'value' in drug_scraper.review_list[-1]
+
+#test for drugs.com 
+#need to review
+def test_drugs_scrape():
+    input = 'https://www.drugs.com/comments/dabigatran/'
+    output_path = 'dabigatran.csv'
+    drugs_scraper = DrugsScraper
+    drugs_scraper.scrape(input, output_path, pages=1)
+    assert drugs_scraper.review_list
+    assert 'comment' and 'for' and 'rating' in drugs_scraper.review_list[-1]
+
+#Test for everydayhealth.com 
+
