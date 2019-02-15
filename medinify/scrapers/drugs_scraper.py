b1eb63912811158b27525df4b585a11b5992f5c4
Mark Groves <markgroves757@gmail.com>
2019-02-15 17:29:44 -0500

    Added max_pages method and test for drugs scraper

--- medinify/scrapers/drugs_scraper.py
+++ medinify/scrapers/drugs_scraper.py
@@ -9,6 +9,31 @@ class DrugsScraper():
     """Scrapes Drugs.com for drug reviews.
     """
 
+    all_pages = True
+    pages = 1
+    review_list = []
+
+    def __init__(self, all_pages=True, pages=1):
+        self.all_pages = all_pages
+        self.pages = pages
+
+    def max_pages(self, drug_url):
+        """Finds number of review pages for this drug.
+
+        Args:
+            drug_url: URL for the first page of reviews.
+        Returns:
+            (int) Highest page number
+        """
+        page = requests.get(drug_url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        total_reviews_text = soup.find('span', {'class': 'totalreviews'}).text
+        total_reviews = [int(s) for s in total_reviews_text.split() if s.isdigit()][0]
+        max_pages = total_reviews // 5
+        print('Found ' + str(total_reviews) + ' reviews.')
+        print('Scraping ' + str(max_pages) + ' pages...')
+        return max_pages
+
     def scrape(self, drug_url, output_path, pages=1):
         """Scrape for drug reviews.
 
@@ -18,9 +43,16 @@ class DrugsScraper():
             pages (int): Number of pages to scrape
         """
 
-        review_list = []
+        print('Scraping Drugs.com...')
 
-        for i in range(pages):
+        self.review_list = []
+        
+        if self.all_pages:
+            num_pages = self.max_pages(drug_url)
+        else:
+            num_pages = pages
+
+        for i in range(num_pages):
             url = drug_url + '?page=' + str(i+1)
             page = requests.get(url)
             soup = BeautifulSoup(page.text, 'html.parser')
@@ -34,11 +66,13 @@ class DrugsScraper():
                 if review.find('div', {'class': 'rating-score'}):
                     rating = float(review.find('div', {'class': 'rating-score'}).text)
 
-                review_list.append({'comment': comment, 'for': review_for, 'rating': rating})
+                self.review_list.append({'comment': comment, 'for': review_for, 'rating': rating})
 
         with open(output_path, 'w') as output_file:
             dict_writer = csv.DictWriter(output_file, ['comment', 'for', 'rating'])
             dict_writer.writeheader()
-            dict_writer.writerows(review_list)
+            dict_writer.writerows(self.review_list)
 
-        print('Reviews scraped: ' + str(len(review_list)))
+        print('Reviews scraped: ' + str(len(self.review_list)))
+
+    
--- tests/test_scrapers.py
+++ tests/test_scrapers.py
@@ -1,5 +1,5 @@
 from medinify.scrapers import WebMDScraper
-
+from medinify.scrapers import DrugsScraper
 
 def test_webmd_max_pages():
     input_url = 'https://www.webmd.com/drugs/drugreview-151652-banzel.aspx?drugid=151652&drugname=banzel'
@@ -19,3 +19,15 @@ def test_webmd_scrape():
     webmd_scraper.scrape(input_url)
     assert len(webmd_scraper.review_list) > 5
     assert 'comment' and 'effectiveness' and 'satisfaction' in webmd_scraper.review_list[-1]
+
+def test_drugs_max_pages():
+    input_url = 'https://www.drugs.com/comments/dabigatran/'
+    drugs_scraper = DrugsScraper()
+    assert drugs_scraper.max_pages(input_url) == 2
+
+def test_drugs_scrape():
+    input_url = 'https://www.drugs.com/comments/dabigatran/'
+    drugs_scraper = DrugsScraper()
+    drugs_scraper.scrape(input_url, 'dabigatran.csv', 4)
+    assert len(drugs_scraper.review_list) > 5
+    assert 'comment' and 'for' and 'rating' in  drugs_scraper.review_list[-1]
