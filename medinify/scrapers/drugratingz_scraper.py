51c5e601cdeed91cfdf601a71bea820c92914709
Gabby Gurdin <ggurdin@gmail.com>
2019-07-21 23:52:52 -0400

    Added functions to drugratingz scraper

--- medinify/scrapers/drugratingz_scraper.py
+++ medinify/scrapers/drugratingz_scraper.py
@@ -1,11 +1,12 @@
-"""Scrapes drugratingz.com for drug reviews.
+"""
+Scrapes drugratingz.com for drug reviews.
 """
 
-import csv
+import re
 import requests
 from bs4 import BeautifulSoup
-import os
 from medinify.scrapers.scraper import Scraper
+import pandas as pd
 
 
 class DrugRatingzScraper(Scraper):
@@ -18,14 +19,52 @@ class DrugRatingzScraper(Scraper):
         :param url: drug reviews page url
         :return:
         """
-        pass
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        drug_name = soup.find('title').text.split()[0]
+        reviews = [x for x in soup.find_all('tr', {'class': 'ratingstableodd'})
+                   if 'class' not in x.find('td').attrs] + \
+                  [x for x in soup.find_all('tr', {'class', 'ratingstableeven'})
+                   if 'class' not in x.find('td').attrs]
+
+        rows = {'comment': []}
+        if 'rating' in self.data_collected:
+            rows['rating'] = []
+        if 'date' in self.data_collected:
+            rows['date'] = []
+        if 'drug' in self.data_collected:
+            rows['drug'] = []
+        if 'user id' in self.data_collected:
+            raise AttributeError('DrugRatingz.com does to contain user ids.')
+        if 'url' in self.data_collected:
+            rows['url'] = []
+
+        for review in reviews:
+            rows['comment'].append(re.sub('\n+|\r+', '', review.find(
+                'span', {'class': 'description'}).text.strip()))
+            if 'rating' in self.data_collected:
+                rating_types = ['effectiveness', 'no side effects', 'convenience', 'value']
+                nums = [int(x.text.strip()) for x in review.find_all('td', {'align': 'center'}) if not x.find('img')]
+                ratings = dict(zip(rating_types, nums))
+                rows['rating'].append(ratings)
+            if 'date' in self.data_collected:
+                date = [x.text.strip().replace(u'\xa0', u' ') for x in review.find_all(
+                    'td', {'valign': 'top'}) if not x.find('a') and 'align' not in x.attrs][0]
+                rows['date'].append(date)
+            if 'drug' in self.data_collected:
+                rows['drug'].append(drug_name)
+            if 'url' in self.data_collected:
+                rows['url'].append(url)
+
+        scraped_data = pd.DataFrame(rows, columns=self.data_collected)
+        self.dataset = self.dataset.append(scraped_data, ignore_index=True)
 
     def scrape(self, url):
         """
         Scrapes all reviews of a given drug
         :param url: drug reviews url
         """
-        pass
+        self.scrape_page(url)
 
     def get_url(self, drug_name):
         """
@@ -33,112 +72,24 @@ class DrugRatingzScraper(Scraper):
         :param drug_name: name of drug being searched for
         :return: drug url on given review forum
         """
-        pass
+        search_url = 'https://www.drugratingz.com/searchResults.jsp?thingname=' + \
+                     drug_name.lower().split()[0] + '&1=&2='
+        search_page = requests.get(search_url)
+        search_soup = BeautifulSoup(search_page.text, 'html.parser')
+        version_urls = []
+        if search_soup.find('td', {'align': 'center'}):
+            tags = search_soup.find_all('td', {'valign': 'middle'})
 
-    def get_urls(self, drug_urls_file, output_file):
-        """
-        Given a text file of drug names, searches for and writes file with review urls
-        :param drug_urls_file: path to text file containing review urls
-        :param output_file: path to file to output urls
-        """
-        pass
+            version_urls = []
+            for tag in tags:
+                if tag.find('a') and 'reviews' in tag.find('a').attrs['href']:
+                    tag_urls = tag.find_all('a')
+                    for element in tag_urls:
+                        if 'reviews' in element.attrs['href']:
+                            version_urls.append('https://www.drugratingz.com' + element.attrs['href'])
 
-    """
-    def scrape(self, drug_url):
-
-        page = requests.get(drug_url)
-        soup = BeautifulSoup(page.text, 'html.parser')
-        comments = [comment.text.strip() for comment in soup.find_all(
-            'span', {'class': 'description'})]
-        ratings = [rating.text.strip() for rating in soup.find_all(
-            'td', {'align': 'center'}) if 'valign' in rating.attrs
-            and rating.text.strip().isdigit()]
-
-        review_list = []
-        ratings_index = 0
-        comment_index = 0
-
-        while ratings_index < len(ratings):
-            effectiveness = ratings[ratings_index]
-            nosideeffects = ratings[ratings_index + 1]
-            convenience = ratings[ratings_index + 2]
-            value = ratings[ratings_index + 3]
-
-            review_list.append({
-                'comment': comments[comment_index],
-                'effectiveness': effectiveness,
-                'no side effects': nosideeffects,
-                'convenience': convenience,
-                'value': value
-            })
-
-            ratings_index = ratings_index + 4
-            comment_index = comment_index + 1
-
-        print('Reviews scraped: ' + str(len(review_list)))
-        return review_list
-
-    def get_drug_urls(self, file_path, output_file):
-
-        drugs = []
-        with open(file_path, 'r') as drug_names:
-            drugs_reader = csv.reader(drug_names)
-            for row in drugs_reader:
-                drugs.append(row[0])
-
-        # search for drug info pages
-        unfound_drugs = []
-        drug_review_urls = {}
-        review_urls = []
-
-        if not os.path.exists('drug_results.pickle'):
-            for drug in drugs:
-                print('Searching for {}'.format(drug))
-                search_url = 'https://www.drugratingz.com/searchResults.jsp?thingname=' + \
-                             drug.lower().split()[0] + '&1=&2='
-                search_page = requests.get(search_url)
-                search_soup = BeautifulSoup(search_page.text, 'html.parser')
-                if search_soup.find('td', {'align': 'center'}):
-                    tags = search_soup.find_all('td', {'valign': 'middle'})
-
-                    if not tags:
-                        unfound_drugs.append(drug)
-                        continue
-
-                    version_urls = []
-                    for tag in tags:
-                        if tag.find('a') and 'reviews' in tag.find('a').attrs['href']:
-                            tag_urls = tag.find_all('a')
-                            for element in tag_urls:
-                                if 'reviews' in element.attrs['href']:
-                                    version_urls.append('https://www.drugratingz.com' + element.attrs['href'])
-
-                    version_urls = list(set(version_urls))
-                    num_versions = len(version_urls)
-                    if num_versions == 1:
-                        drug_review_urls[drug] = version_urls[0]
-                    elif num_versions > 1:
-                        for i in range(num_versions):
-                            drug_review_urls[drug + ' ' + str(i + 1)] = version_urls[i]
-
-            drugs = list(drug_review_urls.keys())
-            for drug in drugs:
-                entry = {'Drug': drug, 'URL': drug_review_urls[drug]}
-                review_urls.append(entry)
-
-        print(str(len(unfound_drugs)) + ' drugs not found')
-        print(unfound_drugs)
-
-        # writes url csv file
-        with open(output_file, 'w') as url_csv:
-            fieldnames = ['Drug', 'URL']
-            writer = csv.DictWriter(url_csv, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(review_urls)
-
-        print('Finished writing!')
-        
-    """
+            version_urls = list(set(version_urls))
+        return version_urls
 
 
 
