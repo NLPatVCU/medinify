8501953a325b79d1476e7022ca4aea0ab80428e2
Gabby Gurdin <ggurdin@gmail.com>
2019-07-22 16:16:53 -0400

    Added function to process ratings:

--- medinify/datasets/process/process.py
+++ medinify/datasets/process/process.py
@@ -5,6 +5,7 @@ from gensim.models import KeyedVectors
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import spacy
 from nltk.corpus import stopwords
+from sklearn.preprocessing import LabelEncoder
 
 
 class Process:
@@ -16,14 +17,20 @@ class Process:
     count_vectors = {}
     tfidf_vectors = {}
     average_embeddings = {}
-    remove_stop_words=True
+    pos_threshold = None
+    neg_threshold = None
+    num_classes = None
+    rating_type = None
 
-    def __init__(self, data, w2v_file, remove_stop_words=True):
+    def __init__(self, data, w2v_file, pos_threshold=4.0, neg_threshold=2.0,
+                 num_classes=2, rating_type='effectiveness'):
         """
         Initializes an instance of the class Process
         for processing review ratings / comments
         :param data: DataFrame containing collected review data
         :param w2v_file: w2v_file: path to word2vec file
+        :param pos_threshold: positive rating threshold
+        :param neg_threshold: negative rating threshold
         """
         self.data = data
         for i, row in data.iterrows():
@@ -36,32 +43,43 @@ class Process:
                 self.ratings.append(ratings)
                 self.comments.append(row['comment'])
 
+        for i, comment in enumerate(self.comments):
+            if type(comment) == float:
+                del self.comments[i]
+                del self.ratings[i]
+
         if w2v_file:
             w2v = KeyedVectors.load_word2vec_format(w2v_file)
             wv = dict(zip(list(w2v.vocab.keys()), w2v.vectors))
             self.w2v = wv
 
+        assert num_classes in [2, 3, 5]
+        self.num_classes = num_classes
+        self.rating_type = rating_type
+        self.pos_threshold = pos_threshold
+        self.neg_threshold = neg_threshold
         self.nlp = spacy.load("en_core_web_sm")
         self.stops = set(stopwords.words('english'))
-        self.remove_stop_words = remove_stop_words
 
     def count_vectorize(self):
         """
         Produces count vectors and assosiated ratings vector
         """
-        vectorizer = CountVectorizer(tokenizer=self.tokenize(remove_stop_words=self.remove_stop_words))
-        count_vectors = vectorizer.fit_transform(self.comments)
+        ratings, indices = self.process_ratings()
+        vectorizer = CountVectorizer(tokenizer=self.tokenize)
+        count_vectors = [[x.todense() for x in vectorizer.fit_transform(self.comments)][i] for i in indices]
         self.count_vectors['data'] = count_vectors
-        # Add function to process target (ratings)
+        self.count_vectors['target'] = ratings
 
     def tfidf_vectorize(self):
         """
         Produces tfidf vectors and assosiated ratings vector
         """
-        vectorizer = TfidfVectorizer(tokenizer=self.tokenize(remove_stop_words=self.remove_stop_words))
-        tfidf_vectors = vectorizer.fit_transform(self.comments)
+        ratings, indices = self.process_ratings()
+        vectorizer = TfidfVectorizer(tokenizer=self.tokenize)
+        tfidf_vectors = [[x.todense() for x in vectorizer.fit_transform(self.comments)][i] for i in indices]
         self.tfidf_vectors['data'] = tfidf_vectors
-        # Add function to process target (ratings)
+        self.tfidf_vectors['target'] = ratings
 
     def average_embedding_vectorize(self):
         """
@@ -71,13 +89,12 @@ class Process:
         average_embeddings = []
         num_discarded = 0
 
-        for i, comment in enumerate(self.comments):
-            print(comment)
+        ratings, indices = self.process_ratings()
+        comments = [self.comments[i] for i in indices]
+
+        for i, comment in enumerate(comments):
             token_embeddings = []
-            if type(comment) == str:
-                tokens = self.tokenize(comment, remove_stop_words=self.remove_stop_words)
-            else:
-                continue
+            tokens = self.tokenize(comment)
             for token in tokens:
                 try:
                     token_embeddings.append(self.w2v[token])
@@ -88,22 +105,63 @@ class Process:
                 continue
             average_embedding = np.average(token_embeddings, axis=0)
             average_embeddings.append(average_embedding)
-            new_ratings.append(self.ratings[i])
+            new_ratings.append(ratings[i])
 
         print('Discarded {} empty comments.'.format(num_discarded))
         self.average_embeddings['data'] = average_embeddings
-        # Add function to process target (ratings)
+        self.average_embeddings['target'] = new_ratings
 
-    def tokenize(self, comment, remove_stop_words=True):
+    def tokenize(self, comment):
         """
         Runs spacy tokenization
         :param comment: comment being tokenized
-        :param remove_stop_words: whether or not to remove stop words
         :return: tokens
         """
-        if remove_stop_words:
-            tokens = [token.text for token in self.nlp.tokenizer(comment)
-                      if token.text not in self.stops and not token.is_punct]
-        else:
-            tokens = [token.text for token in self.nlp.tokenizer(comment) if not token.is_punct]
+
+        tokens = [token.text for token in self.nlp.tokenizer(comment)
+                  if token.text not in self.stops and not token.is_punct]
         return tokens
+
+    def process_ratings(self):
+        """
+        Numerically encodes ratings
+        :return: processed ratings and indices
+        """
+        new_ratings = []
+        indices = []
+
+        if type(self.ratings[0]) == dict:
+            ratings = [float(x[self.rating_type]) for x in self.ratings]
+        else:
+            ratings = self.ratings
+
+        if self.num_classes == 2:
+            for i, rating in enumerate(ratings):
+                if rating >= self.pos_threshold:
+                    new_ratings.append('pos')
+                    indices.append(i)
+                elif rating <= self.neg_threshold:
+                    new_ratings.append('neg')
+                    indices.append(i)
+        elif self.num_classes == 3:
+            for i, rating in enumerate(ratings):
+                if rating >= self.pos_threshold:
+                    new_ratings.append('pos')
+                    indices.append(i)
+                elif rating <= self.neg_threshold:
+                    new_ratings.append('neg')
+                    indices.append(i)
+                elif self.neg_threshold < rating < self.pos_threshold:
+                    new_ratings.append('neutral')
+                    indices.append(i)
+        elif self.num_classes == 5:
+            if not np.amin(ratings) == 1.0 or not np.amax(ratings) == 5.0:
+                raise ValueError('Five classes can only be used with ratings data already '
+                                 'split into 5 star classes')
+            for i, rating in enumerate(ratings):
+                new_ratings.append(str(rating))
+                indices.append(i)
+
+        encoder = LabelEncoder()
+        encoded = encoder.fit_transform(new_ratings)
+        return encoded, indices
