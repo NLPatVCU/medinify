ba5a3fd15c288a53f89e94add00d46d84e74f543
Gabby Gurdin <ggurdin@gmail.com>
2019-07-25 18:13:51 -0400

    Added collection option to cli

--- medinify/__main__.py
+++ medinify/__main__.py
@@ -5,19 +5,32 @@ Medinify Command Line Interface Setup
 
 import argparse
 from medinify.sentiment import Classifier
+from medinify.datasets import Dataset
 
 
-def setup(args):
+def setup_classifier(args):
     """
-    Sets up dataset and pipeline/model since it gets used by every command.
+    Sets up classifier.
     :param args: Argparse args object.
-    :return dataset, model: The dataset and model objects created.
+    :return clf: classifier
     """
     clf = Classifier(args.classifier, data_representation=args.data_representation,
                      neg_threshold=args.neg_threshold, num_classes=args.num_classes,
                      pos_threshold=args.pos_threshold, pos=args.pos, w2v_file=args.word_embeddings)
 
     return clf
+
+
+def setup_dataset(args):
+    """
+    Sets up dataset.
+    :param args: command line arguments
+    :return: Dataset
+    """
+    dataset = Dataset(scraper=args.scraper,
+                      use_user_ids=args.collect_user,
+                      use_urls=args.collect_url)
+    return dataset
 
 
 def train(args, clf):
@@ -58,6 +71,24 @@ def classify(args, clf):
                  output_file=args.output)
 
 
+def collect(args, dataset):
+    """
+    Scrapes reviews data
+    :param args: command line arguments
+    :param dataset: medinify dataset object
+    """
+    if args.names_file:
+        dataset.collect_from_drug_names(args.names_file)
+        dataset.write_file(args.output,
+                           write_user_ids=args.collect_user,
+                           write_urls=args.collect_url)
+    elif args.urls_file:
+        dataset.collect_from_urls(args.urls_file)
+        dataset.write_file(args.output,
+                           write_user_ids=args.collect_user,
+                           write_urls=args.collect_url)
+
+
 def main():
     """
     Main function where initial argument parsing happens.
@@ -71,8 +102,8 @@ def main():
     parser.add_argument('-n', '--num-classes', default=2, help='Number of rating classes.', choices=[2, 3, 5], type=int)
     parser.add_argument('-pt', '--pos-threshold', default=4.0, help='Positive rating threshold.')
     parser.add_argument('-nt', '--neg-threshold', default=2.0, help='Negative rating threshold.')
-    parser.add_argument('-wv', '--word-embeddings', help='Path to file containing pretrained word2vec embeddings ''(If using average emeddings as data representation).')
-    parser.add_argument('-p', '--pos', help='Part of speech ''(If using part of speech count vectors as data representation)')
+    parser.add_argument('-wv', '--word-embeddings', help='Path to file containing pretrained word2vec embeddings (If using average emeddings as data representation).')
+    parser.add_argument('-p', '--pos', help='Part of speech (If using part of speech count vectors as data representation)')
     subparsers = parser.add_subparsers()
 
     # Train arguments
@@ -100,12 +131,30 @@ def main():
     parser_classify.add_argument('-o', '--output', help='Path to save model file', required=True)
     parser_classify.set_defaults(func=classify)
 
+    # Collect arguments
+    parser_collect = subparsers.add_parser('collect', help='Collects drug review data.')
+    parser_collect.add_argument('-s', '--scraper', help='If collecting data, which scraper to use',
+                        choices=['WebMD', 'Drugs', 'DrugRatingz', 'EverydayHealth'])
+    file_path = parser_collect.add_mutually_exclusive_group(required=True)
+    file_path.add_argument('-nf', '--names-file',
+                           help='Path to drug names file.')
+    file_path.add_argument('-uf', '--urls-file',
+                           help='Path to urls file.')
+    parser_collect.add_argument('--collect-user', help='Whether to collect user id data', action='store_true')
+    parser_collect.add_argument('--collect-url', help='Whether to collect url data', action='store_true')
+    parser_collect.add_argument('-o', '--output', help='Path to save model file', required=True)
+    parser_collect.set_defaults(func=collect)
+
     # Parse initial args
     args = parser.parse_args()
 
     # Run proper function
-    clf = setup(args)
-    args.func(args, clf)
+    if args.func in [train, evaluate, validate, classify]:
+        clf = setup_classifier(args)
+        args.func(args, clf)
+    elif args.func == collect:
+        dataset = setup_dataset(args)
+        args.func(args, dataset)
 
 
 if __name__ == '__main__':
