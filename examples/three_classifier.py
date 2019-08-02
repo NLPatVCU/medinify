9c539f6bd6773981b14d87fbeebe99acb3730347
Ishaan <hcps-thakuris@henricostudents.org>
2019-08-02 12:51:16 -0400

    Updated three_classifier

--- examples/three_classifier.py
+++ examples/three_classifier.py
@@ -1,5 +1,7 @@
 # Medinify classifier to sort reviews into negative, neutral and positive reviews
 from medinify.sentiment import ReviewClassifier
+from medinify.sentiment.rnn_review_classifier import RNNReviewClassifier
+from medinify.sentiment.rnn_review_classifier import RNNNetwork
 from medinify.datasets import ReviewDataset
 import sys
 
@@ -18,11 +20,39 @@ def main():
     dataset2.remove_empty_comments()
     dataset2.write_file("csv", "depression.csv")
     """
-
-    three_classifier = ReviewClassifier("nb", 3)
+    """
+    plist=[]
+    three_classifier = ReviewClassifier("rnn", 5, poslist=plist)
     data3, target3 = three_classifier.preprocess('diabetes.csv')
     three_classifier.fit(data3, target3)
     three_classifier.evaluate_average_accuracy('diabetes.csv', 10)
+    """
     #three_classifier.classify('classify.txt', csv_file='diabetes.csv', evaluate=True)
+    """
+    var3, var4 = three_classifier.evaluate(three_network, dataloadervalid)
+    print(str(var) + " " + str(var2 * 100) + "%")
+    print(str(var3) + " " + str(var4 * 100) + "%")
+    """
+    INPUT_DIM = 25000
+    EMBEDDING_DIM = 100
+    HIDDEN_DIM = 256
+    OUTPUT_DIM = 1
+    N_LAYERS = 2
+    BIDIRECTIONAL = True
+    DROPOUT = 0.5
+    
+    three_classifier = RNNReviewClassifier()
+    dataloadertrain, dataloadervalid, padid, unkid = three_classifier.get_data_loaders('diabetes.csv', 'heart.csv', 64)
+    three_network = RNNNetwork(INPUT_DIM, 
+            EMBEDDING_DIM, 
+            HIDDEN_DIM, 
+            OUTPUT_DIM, 
+            N_LAYERS, 
+            BIDIRECTIONAL, 
+            DROPOUT, padid, unkid)
+    
+    three_classifier.train(three_network, dataloadertrain)
+    three_classifier.evaluate2(three_network, dataloadervalid)
+    
 if __name__ == "__main__":
     main()
