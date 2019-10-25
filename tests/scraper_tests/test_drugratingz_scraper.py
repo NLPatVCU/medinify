e42f8cf67b7ac6bd3be1ff4f48881ca562291907
Lois Appiah <loisappiah@Loiss-MacBook-Pro.local>
2019-10-25 15:50:29 -0400

    Added scraper test

--- tests/scraper_tests/test_drugratingz_scraper.py
+++ tests/scraper_tests/test_drugratingz_scraper.py
@@ -1,2 +1,12 @@
 
 # TODO (Dunya) - Add tests for DrugRatingz Scraper
+
+import pytest
+from medinify.scrapers import DrugRatingzScraper
+
+
+def test_default_initialization():
+    scraper = DrugRatingzScraper()
+    assert len(scraper.reviews) == 0
+    assert not scraper.collect_urls
+
