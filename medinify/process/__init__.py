f990b3506312c0feb2bc13577243a88796b39cd5
ggurdin <ggurdin@gmail.com>
2019-11-07 23:03:33 -0500

    Added pos processor

--- medinify/process/__init__.py
+++ medinify/process/__init__.py
@@ -2,3 +2,4 @@ from .processor import Processor
 from .bow_processor import BowProcessor
 from .embeddings_processor import EmbeddingsProcessor, find_embeddings
 from .matrix_processor import MatrixProcessor
+from .pos_processor import PosProcessor
