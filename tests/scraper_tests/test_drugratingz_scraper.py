e1b130f9a986da4529dd5045f08b23921b6b9394
Lois Appiah <loisappiah@Loiss-MacBook-Pro.local>
2019-11-08 14:46:16 -0500

    Documented the scraper

--- tests/scraper_tests/test_drugratingz_scraper.py
+++ tests/scraper_tests/test_drugratingz_scraper.py
@@ -1,39 +1,56 @@
 
-# TODO (Dunya) - Add tests for DrugRatingz Scraper
 
 import pytest
 from medinify.scrapers import DrugRatingzScraper
 
-def tautology_test():
-
-    useless = True
-    assert useless == True
-
 
 def test_default_initialization():
+    """
+    Tests that when the new scraper object is initialized, the correct
+    attributes are set (empty list[dict] 'reviews') and the attribute
+    collect_urls (boolean) is false by default
+    """
     scraper = DrugRatingzScraper()
     assert len(scraper.reviews) == 0
     assert not scraper.collect_urls
 
 
 def test_initialization_parameters():
+    """
+    Tests that when a new DrugRatingz scraper object is initialized with
+    non default arguments the correct attributes are set (empty list[dict] 'reviews')
+    and that the collect_urls attribute (boolean) is set to true
+    """
     scraper = DrugRatingzScraper(collect_urls=True)
     assert scraper.collect_urls
 
 
 def test_incorrect_url():
+    """
+    Tests that the scrape_page function raises and assertion error when a url is
+    entered with an incorrect domain name (not 'https://drugratingz.com...')
+    """
     scraper = DrugRatingzScraper()
     with pytest.raises(AssertionError):
         scraper.scrape_page('https://www.drugs.com/comments/aripiprazole/abilify.html?page=1')
 
 
 def test_no_reviews():
+    """
+    Tests that the scrape page function returns 'None' when no reviews are
+    found on the page
+    """
     scraper = DrugRatingzScraper()
     returned = scraper.scrape_page('https://www.drugratingz.com/reviews/18088/Drug-Affinitor.html')
-    assert returned == None
+    assert not returned
 
 
 def test_scrape_correct_review_data():
+    """
+    Tests to make sure that the last review in the scraped reviews list has
+    the correct data when the scrape function is called
+    (this data is from the oldest review of the drug)
+    """
     scraper = DrugRatingzScraper(collect_urls=True)
     scraper.scrape('https://www.drugratingz.com/reviews/141/Drug-Prednisone.html')
     assert scraper.reviews[-1]['comment'][:10] == 'This is a '
@@ -46,6 +63,11 @@ def test_scrape_correct_review_data():
 
 
 def test_scrape_page_default_parameters():
+    """
+    Tests to make sure that calling the scrape_page function on a scraper object
+    with a default parameter collects the correct types of data ('comment', 'rating',
+    'date', and 'drug') and that the correct number of reviews were collected (13)
+    """
     scraper = DrugRatingzScraper()
     scraper.scrape_page('https://www.drugratingz.com/reviews/141/Drug-Prednisone.html')
     data_collected = list(scraper.reviews[0].keys())
@@ -58,6 +80,11 @@ def test_scrape_page_default_parameters():
 
 
 def test_scrape_page_with_parameters():
+    """
+    Tests to make sure that calling the scrape_page function on a scraper object with a
+    non-default parameter (collect_url true) collects the correct types of data
+    ('comment', 'rating, 'date', 'drug', and 'url')
+    """
     scraper = DrugRatingzScraper(collect_urls=True)
     scraper.scrape_page('https://www.drugratingz.com/reviews/141/Drug-Prednisone.html')
     data_collected = list(scraper.reviews[0].keys())
@@ -66,6 +93,10 @@ def test_scrape_page_with_parameters():
 
 
 def test_scrape_empty_reviews():
+    """
+    Tests to make sure that the scrape function would discard the reviews
+    of a scraper object that already has data collected in 'reviews'
+    """
     scraper = DrugRatingzScraper()
     scraper.scrape('https://www.drugratingz.com/reviews/258/Drug-Glucophage.html')
     num_reviews = len(scraper.reviews)
@@ -74,12 +105,21 @@ def test_scrape_empty_reviews():
 
 
 def test_scrape_invalid_url_no_title():
+    """
+    Tests that when the scrape function is called on a url that lacks a title
+    (invalid url), it raises an AttributeError and returns 'None'
+    """
     scraper = DrugRatingzScraper()
     returned = scraper.scrape('https://www.drugratingz.com/reviews/258/Drug.html')
-    assert returned == None
+    assert not returned
 
 
 def test_scrape_default_parameter():
+    """
+    Tests that, when calling the scrape function with a scraper with default parameters,
+    the correct types of data are stored in the 'reviews' attribute and that the
+    correct number of reviews are collected (all of the reviews on the infinite page)
+    """
     scraper = DrugRatingzScraper()
     scraper.scrape('https://www.drugratingz.com/reviews/472/Drug-Lutera.html')
     assert len(scraper.reviews) > 10
@@ -92,6 +132,10 @@ def test_scrape_default_parameter():
 
 
 def test_scrape_with_parameters():
+    """
+    Tests that, when calling the scrape function with a scraper of non-default parameters, the
+    correct types of data are stored in the 'reviews' attribute
+    """
     scraper = DrugRatingzScraper(collect_urls=True)
     scraper.scrape('https://www.drugratingz.com/reviews/472/Drug-Lutera.html')
     assert len(scraper.reviews) > 10
@@ -101,50 +145,61 @@ def test_scrape_with_parameters():
 
 
 def test_scrape_assert_title_error():
+    """
+    Tests that when the scrape function is called with an invalid url that does have a
+    title, but the title is wrong (doesn't have the phrase 'drug reviews') that an AssertionError
+    is raised and the function returns 0
+    """
     scraper = DrugRatingzScraper()
     returned = scraper.scrape('https://www.drugratingz.com/ShowThingCats.jsp')
     assert returned == 0
 
 
 def test_scrape_no_reviews():
+    """
+    Tests that the scrape function works for page with no reviews
+    """
     scraper = DrugRatingzScraper()
     scraper.scrape('https://www.drugratingz.com/reviews/18589/Drug-Adderall.html')
     assert len(scraper.reviews) == 0
 
 
-def test_get_rul_real_drug_name():
+def test_get_url_real_drug_name():
+    """
+    Tests that the get_url function returns the correct url for a standard drug name ('actos')
+    """
     scraper = DrugRatingzScraper()
     url = scraper.get_url('actos')
     assert url == 'https://www.drugratingz.com/reviews/340/Drug-Actos.html'
 
 
 def test_url_fake_drug_name():
+    """
+    Tests tha the get_url function returns 'None' for a drug name that does not have a review
+    page
+    """
     scraper = DrugRatingzScraper()
     url = scraper.get_url('garbage')
     assert not url
 
 
 def test_drug_name_with_space():
+    """
+    Tests that the get_url function returns the correct url for a drug name
+    that includes a space
+    """
     scraper = DrugRatingzScraper()
     url = scraper.get_url('Ortho Tri-Cyclen Lo ')
     assert url == 'https://www.drugratingz.com/reviews/163/Drug-Ortho-Tri-Cyclen-Lo.html'
 
 
-
 def test_short_drug_name():
+    """
+    Tests that the get_url function does not search for drug names shorter than 4 characters
+    """
     scraper = DrugRatingzScraper()
     url = scraper.get_url('ACE')
     assert not url
 
 
-def test_name_with_numbers():
-    scraper = DrugRatingzScraper()
-    url = scraper.get_url('FPE 1070 (IS) Fpf 1070 (IS)')
-    assert url == 'https://www.drugratingz.com/reviews/18813/Drug-Renacenz.html'
-
-
-def test_name_with_numbers_and_spaces():
-    scraper = DrugRatingzScraper()
-    url = scraper.get_url('Ortho Cyclen 28')
-    assert url == 'https://www.drugratingz.com/reviews/229/Drug-Ortho-Cyclen-28.html'
 
