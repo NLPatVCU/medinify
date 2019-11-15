ff49d13af72e441517e25fc8257c15b4f0193c79
ggurdin <ggurdin@gmail.com>
2019-11-15 17:16:09 -0500

    Fixed import order

--- medinify/classifiers/__init__.py
+++ medinify/classifiers/__init__.py
@@ -1,10 +1,11 @@
 
+from .data_iterator import DataIterator
 from .cnn_learner import CNNLearner
 from .cnn_learner import ClassificationNetwork
-from .classifier import Classifier
 from .model import Model
+from .classifier import Classifier
 from .utils import print_evaluation_metrics
 from .utils import print_validation_metrics
 from .utils import find_model
-from .data_iterator import DataIterator
+
 
